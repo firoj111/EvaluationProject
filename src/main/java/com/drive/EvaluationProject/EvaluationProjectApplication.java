03c7ac73394601187732951037a3bd088f4e90ce
firoj111 <mohdfirojalam9149@gmail.com>
2025-05-18 18:15:44 +0530

    Initial Commit

---
+++ src/main/java/com/drive/EvaluationProject/EvaluationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.drive.EvaluationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationProjectApplication.class, args);
+	}
+
+}
